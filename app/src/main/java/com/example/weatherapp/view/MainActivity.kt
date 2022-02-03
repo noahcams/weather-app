ee05e2632d2772bfbb7d116b0a103746c53621e2
RAVE43 <rave43@RAVE43s-MacBook-Air.local>
2022-02-03 16:25:26 -0500

    Fixed a binding issue in MainActivity.onCreate

--- app/src/main/java/com/example/weatherapp/view/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/view/MainActivity.kt
@@ -1,7 +1,54 @@
 package com.example.weatherapp.view
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.weatherapp.R
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.viewpager.widget.ViewPager
+import com.example.weatherapp.databinding.ActivityMainBinding
 
 
-class MainActivity : AppCompatActivity(R.layout.activity_main)
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    /**
+     * The pager widget, which handles animation and allows swiping horizontally to access previous
+     * and next wizard steps.
+     */
+    private lateinit var mPager: ViewPager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        // Instantiate a ViewPager and a PagerAdapter.
+        mPager = binding.viewPager
+
+      // The pager adapter, which provides the pages to the view pager widget.
+        val pagerAdapter = ScreenSlidePagerAdapter(supportFragmentManager)
+        mPager.adapter = pagerAdapter
+  }
+
+    override fun onBackPressed() {
+        if (mPager.currentItem == 0) {
+            // If the user is currently looking at the first step, allow the system to handle the
+            // Back button. This calls finish() on this activity and pops the back stack.
+            super.onBackPressed()
+        } else {
+            // Otherwise, select the previous step.
+            mPager.currentItem = mPager.currentItem - 1
+        }
+    }
+
+  /**
+   * A simple pager adapter that represents 5 ScreenSlidePageFragment objects, in
+   * sequence.
+   */
+    private inner class ScreenSlidePagerAdapter(fm: FragmentManager) :
+        FragmentStatePagerAdapter(fm) {
+        override fun getCount(): Int = 1
+
+        override fun getItem(position: Int): Fragment = HomeFragment()
+    }
+}
